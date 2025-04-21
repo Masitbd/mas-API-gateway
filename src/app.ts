8a809895a5ce1acc40952895b365d42d57e6de42
Maruf-9270 <marufahmed9270@gmail.com>
2025-04-21 10:36:58 +0600

    Fiex

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app: Application = express();
 
 app.use(
   cors({
-    origin: 'https://mas-hms-frontend.vercel.app',
+    origin: 'http://148.135.137.151:9006',
     credentials: true
   })
 );
